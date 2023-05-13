116991539bb01d12c788df244ef9bf8cc387071f
shaojiang.wang <shaojiang.wang@datayes.com>
2023-05-13 15:31:24 +0800

    chore: reorganize project directory, add module folder, component in content only as entrance

--- contents/popover.tsx
+++ contents/popover.tsx
@@ -1,130 +1,17 @@
-import { useCallback, useEffect, useState } from 'react'
 
-import styleText from 'data-text:./popover.module.less'
-import * as style from './popover.module.less'
-
-import { useKeyPress, useRequest } from 'ahooks'
-import { sendToBackground } from '@plasmohq/messaging'
-
-import useTextSelection from '~hooks/useTextSelection'
-import type { TranslateResult } from '~types/translate'
+import PopoverContainer, { getStyle } from '~/modules/popover/popover.container'
 
 function PopoverIndex () {
-  const [mode, setMode] = useState<'icon' | 'panel'>('icon')
-  const [isOpen, setIsOpen] = useState(false)
-  const [textSelection, clearSelection] = useTextSelection()
-
-  const { data, loading, run } = useRequest(
-    async ({ text, sentence }: { text: string, sentence: string }): Promise<TranslateResult> => {
-      const result = await sendToBackground({
-        name: 'translate',
-        body: {
-          text,
-          sentence
-        }
-      })
-      return result
-    },
-    { manual: true }
-  )
-
-  console.log(data)
-
-  useEffect(() => {
-    if (textSelection) {
-      setIsOpen(true)
-      setMode('icon')
-    }
-  }, [textSelection])
-
-  const handleClosePanel = useCallback(() => {
-    setIsOpen(false)
-    clearSelection()
-    setMode('icon')
-  }, [clearSelection])
-
-  useKeyPress('esc', () => {
-    handleClosePanel()
-  })
-
-  const handleClickIcon = useCallback(() => {
-    if (!textSelection) return
-    setMode('panel')
-    run({ text: textSelection.text, sentence: textSelection.sentence })
-  }, [run, textSelection])
-
-  if (!isOpen || !textSelection) {
-    return null
-  }
-
-  const position = {
-    top: `${textSelection.rect.top + 30}px`,
-    left: `${textSelection.rect.left}px`
-  }
-
-  const renderPanel = () => {
-    if (loading) {
-      return 'loading...'
-    }
-    if (data) {
-      return (
-      <>
-        <div>{textSelection.text}</div>
-        <div>释义: {data.explain}</div>
-        <div>{data.part_of_speech.map(i => <div key={i.type}>{i.type}: {i.explain}</div>)}</div>
-        <p className={style.tense}>
-          <div>复数形式: {data.third_person_singular}</div>
-          <div>第三人称单数: {data.third_person_singular}</div>
-          <div>过去式: {data.past_tense}</div>
-          <div>过去分词: {data.past_participle}</div>
-          <div>现在分词: {data.present_participle}</div>
-        </p>
-        <p>
-          {data.example_sentences.map(i => (<div key={i.english}>
-            <div>{i.english} </div>
-            <div>{i.chinese}</div>
-          </div>))}
-        </p>
-        <p>
-        <div>专业术语</div>
-        {data.computer_science_definition && <div>计算机:{data.computer_science_definition}</div>}
-        {data.finance_definition && <div>金融:{data.computer_science_definition}</div>}
-        </p>
-
-      </>)
-    }
-  }
-
-  return mode === 'icon'
-    ? (
-      <div
-        className={style.pin}
-        onClick={handleClickIcon}
-        style={{
-          ...position
-        }}
-      />
-      )
-    : (
-      <div
-        className={style.panel}
-        style={{
-          ...position
-        }}
-      >
-        {renderPanel()}
-      </div>
-      )
+  return <PopoverContainer/>
 }
 
 export default PopoverIndex
 
-export const config = {
+const config = {
   matches: ['http://*/*', 'https://*/*', '<all_urls>']
 }
 
-export const getStyle = () => {
-  const style = document.createElement('style')
-  style.textContent = styleText
-  return style
+export {
+  getStyle,
+  config
 }
