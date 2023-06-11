0a0992b1c717732a82fe170d700f799412b8e82d
shaojiang.wang <shaojiang.wang@datayes.com>
2023-06-12 07:10:43 +0800

    feat: 初步添加收藏功能

--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
 /* eslint-disable no-undef */
-export const tabManageUrl = `chrome-extension://${chrome.runtime.id}/tabs/tab-manage.html`
+export const tabManageUrl = `chrome-extension://${chrome.runtime.id}/tabs/wordbook.html`
--- modules/popover/popover.container.tsx
+++ modules/popover/popover.container.tsx
@@ -1,37 +1,59 @@
+import {
+  LeftOutlined,
+  PhoneOutlined,
+  RightOutlined,
+  StarOutlined
+} from '@ant-design/icons'
+import styled from '@emotion/styled'
+import { useKeyPress, useRequest } from 'ahooks'
+import { Input } from 'antd'
+import styleText from 'data-text:./popover.module.less'
 import { useCallback, useEffect, useState } from 'react'
 
-import styleText from 'data-text:./popover.module.less'
-import * as style from './popover.module.less'
-
-import { useKeyPress, useRequest } from 'ahooks'
 import { sendToBackground } from '@plasmohq/messaging'
+import { useStorage } from '@plasmohq/storage/hook'
 
 import useTextSelection from '~hooks/useTextSelection'
 import type { TranslateResult } from '~types/translate'
-import { PhoneOutlined } from '@ant-design/icons'
-import styled from '@emotion/styled'
+
+import * as style from './popover.module.less'
 
 const StyledHeader = styled.div`
   display: flex;
   height: 30px;
   width: 100%;
   background-color: green;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const StyledStarOutlined = styled(StarOutlined)<{ isFavorite }>`
+  color: ${(props) => (props.isFavorite ? 'red' : 'white')};
+  font-size: 16px;
 `
 
 const StyledContent = styled.div`
- padding: 10px;
+  padding: 10px;
 `
 
 function PopoverContainer () {
   const [mode, setMode] = useState<'icon' | 'panel'>('icon')
+
+  const [inputValue, setInputValue] = useState<string| undefined>()
+
+  const [favorites, setFavorites] = useStorage<string[]>('favorites', [])
+
   const [isOpen, setIsOpen] = useState(false)
   const [textSelection, clearSelection] = useTextSelection()
 
-  const defaultApiKey = process.env.PLASMO_PUBLIC_SHIP_NAME
-  console.log('PLASMO_PUBLIC_SHIP_NAME', defaultApiKey)
-
   const { data, loading, run } = useRequest(
-    async ({ text, sentence }: { text: string, sentence: string }): Promise<TranslateResult> => {
+    async ({
+      text,
+      sentence
+    }: {
+      text: string
+      sentence: string
+    }): Promise<TranslateResult> => {
       const result = await sendToBackground({
         name: 'translate',
         body: {
@@ -44,14 +66,16 @@ function PopoverContainer () {
     { manual: true }
   )
 
-  console.log(data)
-
   useEffect(() => {
     if (textSelection) {
       setIsOpen(true)
       setMode('icon')
     }
   }, [textSelection])
+
+  useEffect(() => {
+    setInputValue(textSelection?.text)
+  }, [textSelection?.text])
 
   const handleClosePanel = useCallback(() => {
     setIsOpen(false)
@@ -69,14 +93,20 @@ function PopoverContainer () {
     run({ text: textSelection.text, sentence: textSelection.sentence })
   }, [run, textSelection])
 
-  const handleSpeech = useCallback(
-    (text: string, lang = 'en-US') => {
-      const utterance = new SpeechSynthesisUtterance(text)
-      utterance.lang = lang
-      speechSynthesis.speak(utterance)
-    },
-    []
-  )
+  const handleSpeech = useCallback((text: string, lang = 'en-US') => {
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.lang = lang
+    speechSynthesis.speak(utterance)
+  }, [])
+
+  const handleToggleFavorites = useCallback(() => {
+    if (!textSelection) return
+    if (favorites.includes(textSelection?.text)) {
+      setFavorites(favorites.filter((i) => i !== textSelection?.text))
+    } else {
+      setFavorites([...favorites, textSelection?.text])
+    }
+  }, [favorites, setFavorites, textSelection])
 
   if (!isOpen || !textSelection) {
     return null
@@ -93,57 +123,103 @@ function PopoverContainer () {
     }
     if (data) {
       return (
-      <>
-      <StyledHeader/>
-      <StyledContent>
-        <div>{textSelection.text}</div>
-        <div>释义: {data.explain}</div>
-        <div>uk: {data.phonetic.uk} <PhoneOutlined rev={''} onClick={() => handleSpeech(textSelection.text, 'en-GB')} /></div>
-        <div>us: {data.phonetic.us} <PhoneOutlined rev={''} onClick={() => handleSpeech(textSelection.text)} /></div>
-        <div>{data.part_of_speech.map(i => <div key={i.type}>{i.type}: {i.explain}</div>)}</div>
-        <p className={style.tense}>
-          <div>复数形式: {data.third_person_singular}</div>
-          <div>第三人称单数: {data.third_person_singular}</div>
-          <div>过去式: {data.past_tense}</div>
-          <div>过去分词: {data.past_participle}</div>
-          <div>现在分词: {data.present_participle}</div>
-        </p>
-        <p>
-          {data.example_sentences.map(i => (
-          <div key={i.english}>
-            <div><PhoneOutlined onClick={() => handleSpeech(i.english)} rev={undefined} /> {i.english}</div>
-            <div>{i.chinese}</div>
-          </div>))}
-        </p>
-        <p>
-        <div>专业术语</div>
-        {data.computer_science_definition && <div>计算机:{data.computer_science_definition}</div>}
-        {data.finance_definition && <div>金融:{data.computer_science_definition}</div>}
-        </p>
-        </StyledContent>
-      </>)
+        <>
+          <StyledHeader>
+            <div>
+              <div>
+                <LeftOutlined rev={undefined} />
+                <RightOutlined rev={undefined} />
+              </div>
+              <Input.Search value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+            </div>
+            <div>
+              <StyledStarOutlined
+                isFavorite={
+                  textSelection?.text && favorites.includes(textSelection.text)
+                }
+                rev={''}
+                onClick={handleToggleFavorites}
+              />
+            </div>
+          </StyledHeader>
+          <StyledContent>
+            <div>{textSelection.text}</div>
+            <div>释义: {data.explain}</div>
+            <div>
+              uk: {data.phonetic.uk}{' '}
+              <PhoneOutlined
+                rev={''}
+                onClick={() => handleSpeech(textSelection.text, 'en-GB')}
+              />
+            </div>
+            <div>
+              us: {data.phonetic.us}{' '}
+              <PhoneOutlined
+                rev={''}
+                onClick={() => handleSpeech(textSelection.text)}
+              />
+            </div>
+            <div>
+              {data.part_of_speech.map((i) => (
+                <div key={i.type}>
+                  {i.type}: {i.explain}
+                </div>
+              ))}
+            </div>
+            <p className={style.tense}>
+              <div>复数形式: {data.third_person_singular}</div>
+              <div>第三人称单数: {data.third_person_singular}</div>
+              <div>过去式: {data.past_tense}</div>
+              <div>过去分词: {data.past_participle}</div>
+              <div>现在分词: {data.present_participle}</div>
+            </p>
+            <p>
+              {data.example_sentences.map((i) => (
+                <div key={i.english}>
+                  <div>
+                    <PhoneOutlined
+                      onClick={() => handleSpeech(i.english)}
+                      rev={undefined}
+                    />{' '}
+                    {i.english}
+                  </div>
+                  <div>{i.chinese}</div>
+                </div>
+              ))}
+            </p>
+            <p>
+              <div>专业术语</div>
+              {data.computer_science_definition && (
+                <div>计算机:{data.computer_science_definition}</div>
+              )}
+              {data.finance_definition && (
+                <div>金融:{data.computer_science_definition}</div>
+              )}
+            </p>
+          </StyledContent>
+        </>
+      )
     }
   }
 
   return mode === 'icon'
     ? (
-      <div
-        className={style.pin}
-        onClick={handleClickIcon}
-        style={{
-          ...position
-        }}
-      />
+    <div
+      className={style.pin}
+      onClick={handleClickIcon}
+      style={{
+        ...position
+      }}
+    />
       )
     : (
-      <div
-        className={style.panel}
-        style={{
-          ...position
-        }}
-      >
-        {renderPanel()}
-      </div>
+    <div
+      className={style.panel}
+      style={{
+        ...position
+      }}>
+      {renderPanel()}
+    </div>
       )
 }
 
