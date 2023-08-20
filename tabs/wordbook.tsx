4e5abeedbfe771240114f51e498f647fe6130963
shaojiang.wang <shaojiang.wang@datayes.com>
2023-08-20 18:28:57 +0800

    chore: add trpc clint

--- tabs/wordbook.tsx
+++ tabs/wordbook.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react"
+
 import { useStorage } from "@plasmohq/storage/hook"
+
+import { api } from "~serivices/api"
 
 function TabManage() {
   const [favorites] = useStorage<string[]>("favorites", [])
 
-  return <div>{JSON.stringify(favorites)}</div>
+  useEffect(() => {
+    const hhh = async () => {
+      console.log("---------")
+      const users = await api.word.hello.query({ text: "from tRPC" })
+      console.log(users, "hello")
+    }
+    hhh()
+  }, [])
+
+  return (
+    <div>
+      wrwqetq
+      {JSON.stringify(favorites)}
+    </div>
+  )
 }
 
 export default TabManage
